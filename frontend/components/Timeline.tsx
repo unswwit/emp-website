a30d2abbd196946f0ce6dd18a27b5e95b035e5b8
ItsLame <37344470+ItsLame@users.noreply.github.com>
2023-04-01 12:32:53 +1100

    feat(timeline): tabs and container (not styled)
    * imported mui library
    + created list and calendar tabs

--- frontend/components/Timeline.tsx
+++ frontend/components/Timeline.tsx
@@ -1,17 +1,59 @@
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import * as React from "react";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+
+function TimelineTabs() {
+  const [value, setValue] = React.useState("1");
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "100%", typography: "body1" }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <TabList onChange={handleChange} aria-label="timeline tabs">
+            <Tab label="List" value="1" />
+            <Tab label="Calendar" value="2" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">
+          <TimelineList />
+        </TabPanel>
+        <TabPanel value="2">
+          <TimelineCalendar />
+        </TabPanel>
+      </TabContext>
+    </Box>
+  );
+}
+
+function TimelineList() {
+  return <div>Timeline stuff</div>;
+}
+
+function TimelineCalendar() {
+  return <div>Calendar stuff</div>;
+}
 
 export default function Timeline() {
   return (
     <div className={styles.wrapper}>
-      <Image
+      {/* <Image
         src="/timeline.png"
         alt="Timeline"
         className={styles.heroImage}
         width={1100}
         height={130}
         priority
-      />
+      /> */}
+      <TimelineTabs />
     </div>
-  )
+  );
 }
