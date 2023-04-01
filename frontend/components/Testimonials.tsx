d933e9cf05b160a0e7a13238f88bbee75bf6495b
gracexkan <80564637+gracexkan@users.noreply.github.com>
2023-04-02 00:50:22 +1100

    chore(utils): add linting to GitHub workflow [WIT-97] (#5)
    * refactor: add yml file for lint
    
    * refactor: add necessary files for eslint and prettier
    
    * chore: renamed from Jest to Lint'
    
    * added necessary files into correct folders
    
    * chore: made working directory frontend
    
    * chore: add prettier by installing it
    
    * chore: see if renaming prettier works
    
    * chore: made typo with file name
    
    * chore: remove eslintcache
    
    * chore: installed prettier again
    
    * chore: added plugins
    
    * chore: finally fixed lint
    
    * refactor: fixed linting

--- frontend/components/Testimonials.tsx
+++ frontend/components/Testimonials.tsx
@@ -1,17 +1,17 @@
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 export default function Testimonials() {
-    return (
-      <div className={styles.wrapper}>
-        <Image
-          src="/testimonials.png"
-          alt="testimonials"
-          className={styles.heroImage}
-          width={1100}
-          height={550}
-          priority
-        />
-      </div>
-    )
-  }
+  return (
+    <div className={styles.wrapper}>
+      <Image
+        src="/testimonials.png"
+        alt="testimonials"
+        className={styles.heroImage}
+        width={1100}
+        height={550}
+        priority
+      />
+    </div>
+  );
+}
