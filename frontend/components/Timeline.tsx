1853918fbb99d96f0d51ef7ff09c37c0d40769e0
ItsLame <37344470+ItsLame@users.noreply.github.com>
2023-04-07 15:41:11 +1000

    fix(timeline): container padding

--- frontend/components/Timeline.tsx
+++ frontend/components/Timeline.tsx
@@ -43,7 +43,7 @@ function TabPanel(props: TabPanelProps) {
       aria-labelledby={`tab-${index}`}
       {...other}
     >
-      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+      {value === index && <Box sx={{ pt: 3 }}>{children}</Box>}
     </div>
   );
 }
