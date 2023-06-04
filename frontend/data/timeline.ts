763af246d458c5d10ba6a9d361dd53dec26ec363
ItsLame <37344470+ItsLame@users.noreply.github.com>
2023-06-04 10:16:11 +1000

    feat(frontend): timeline component [wit-20] (#14)
    * feat(timeline): tabs and container (not styled)
    
    * imported mui library
    + created list and calendar tabs
    
    * feat(timeline): calendar (work in progress)
    
    * tried using 2 calendars:
       + toast ui calendar (currently commented out)
       + full calendar
    + some timeline dummy data
    * note: might ditch this idea (?)
    
    * feat(timeline): calendar styled (work in progress)
    
    + added style component for calendar fc
    
    * feat(timeline): tabs styled
    
    + added styled component for tabs/tab
    * switched from experimental @mui/lab tab components to regular ones
    
    * feat(timeline): unstyled info panel + other changes
    
    - removed ToastUI Calendar and related functions/components
    + added info panel
    + display correct data on info panel (wip: days left, labels)
    + added on event action for calendar to open up info panel with correct event
    * info panel structured-ish, stylesheet wip
    
    * feat(timeline): styled info panel
    
    + updated timeline stylesheet
    + added style to info panel
    + info panel configured to be mobile friendly (I hope)
    
    * feat(timeline): info panel days left counter
    
    + added days left counter:
       + starting in n days (for multiple days event)
       + ending in n days (for ongoing multiple days event)
       + last day (for ongoing multiple days event)
       + today (for 1 day event)
       + ended (for any events)
    
    * feat(timeline): info panel labels
    
    + mapped event labels
    
    * feat(timeline): swipe to close info panel
    
    * fix(timeline): font
    
    * montserrat via css instead of from next for info panel (fixes the need to 're-render' via resolution switching, to show correct font')
    * fixed some console warning related to typography
    - removed mui/typography
    * commented out import timeline ssr false to import normally instead
    
    * fix(timeline): container padding
    
    * fix(timeline): style fixes and updates
    
    + updated info panel parameters so that when events are sorted, data won't jumble up (i.e., clicking an item from the list won't bring up the wrong info panel)
    
    + updated styles for improved mobile support
    
    * fix(timeline): calendar height
    
    * chore(timeline): linting
    
    * fix: package-lock missing commas and braces
    
    * feat(frontend): timeline component [wit-20]
    
    * refactor: inline styles to external stylesheet
    
    * refactor: timeline list and card
    
    + separate TimelineList to TimelineList and Card for readability
       * TimelineList handles mapping
       * TimelineCard is the card itself
    + changes to date
       * reduce need of new Date
       * readibility
    + changes to location display
       * ONLINE EVENT to show ONLINE instead
       * For null location show hyphen instead
    
    * fix: timeline list view responsiveness
    
    * feat: list view onclick event open info panel
    
    * feat: list view pagination
    
    * feat: sort list by latest
    
    * feat: list view style update and hover style
    
    * chore: remove commented code
    
    ---------
    
    Co-authored-by: Daniel <ledanielhuynh@gmail.com>

---
+++ frontend/data/timeline.ts
@@ -0,0 +1,137 @@
+export const timeline = [
+  {
+    title: 'Lorem Ipsum Party',
+    start: '2023-04-05T10:00:00',
+    end: '2023-04-07T21:30:00',
+    data: {
+      link: 'https://www.facebook.com/events/579873087508452',
+      location: 'Online Event',
+      labels: ['upskilling', 'workshop'],
+      description:
+        "WIT Presents: Mock Technical Interview Workshop\nHave a technical interview coming up? Not sure how to prepare?\n\nRegister for WIT'S Mock Interview Workshop to hear about tips & tricks on how to ace technical interviews and book a session for a one-on-one mock technical interview with an industry representative. Get ready to receive specific and personalised feedback on how you can improve your tech interview game with industry experts from Airwallex!\n\nNOTE: For this event, you will only be required to attend the opening presentation and the session you have been allocated. If a registrant does not show up, the position will be given to the student next in line. You can also register to attend the opening presentation only.\n\nRegistration link: https://forms.gle/G2LNTQXgg59WCuMMA",
+      photo: {
+        src: 'https://s3-alpha-sig.figma.com/img/b856/ad93/ae397a70982a1f7f1d441a1786b9d6a1?Expires=1681689600&Signature=ifoyuipBICYbQkBeWCCOmbXw4E4bmipnrhDfNgm4nVcUsevP0BhC1X5Vrwl8C~To-38k0LIA5rFQhAKl6-Xbu0Kh72Xejzq37Z9K4sPgX3RNjMMvaCsw8a6cO7mVXUQZet4CXTAXz6H3mTtxlZVbBZgkFa9X00VSprmQaJ4OfsP5xLoRaGkKIQxBQsjppc6HxrKdGGAVM90fJ3lmsgcFUnFOVMFch79~JsFp8DapPD8Z5ozR1Mkxv6sCK1lNvXzNWV8TAIFdCmmVTVXffi-8DjpZsSOGFROdUJ9l7ViY1-naN03y1EzWKqcMQFdqlQvFi7eIPizByQdckFk3Niw7dw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+        alt: 'lorem-ipsum-party',
+      },
+    },
+  },
+  {
+    title: 'Mock Technical Interview Workshop',
+    start: '2023-04-20T15:00:00',
+    end: '2023-04-25T15:30:00',
+    data: {
+      link: '',
+      location: 'Online Event',
+      labels: ['upskilling', 'workshop'],
+      description:
+        "WIT Presents: Mock Technical Interview Workshop\nHave a technical interview coming up? Not sure how to prepare?\n\nRegister for WIT'S Mock Interview Workshop to hear about tips & tricks on how to ace technical interviews and book a session for a one-on-one mock technical interview with an industry representative. Get ready to receive specific and personalised feedback on how you can improve your tech interview game with industry experts from Airwallex!\n\nNOTE: For this event, you will only be required to attend the opening presentation and the session you have been allocated. If a registrant does not show up, the position will be given to the student next in line. You can also register to attend the opening presentation only.\n\nRegistration link: https://forms.gle/G2LNTQXgg59WCuMMA",
+      photo: {
+        src: '',
+        alt: '',
+      },
+    },
+  },
+  {
+    title: 'Super Long Description',
+    start: '2023-04-12T15:00:00',
+    end: '2023-04-12T18:30:00',
+    data: {
+      link: '',
+      location: '',
+      labels: ['lorem', 'ipsum'],
+      description:
+        "WIT Presents: Mock Technical Interview Workshop\nHave a technical interview coming up? Not sure how to prepare?\n\nRegister for WIT'S Mock Interview Workshop to hear about tips & tricks on how to ace technical interviews and book a session for a one-on-one mock technical interview with an industry representative. Get ready to receive specific and personalised feedback on how you can improve your tech interview game with industry experts from Airwallex!\n\nNOTE: For this event, you will only be required to attend the opening presentation and the session you have been allocated. If a registrant does not show up, the position will be given to the student next in line. You can also register to attend the opening presentation only.\n\nRegistration link: https://forms.gle/G2LNTQXgg59WCuMMA\nWIT Presents: Mock Technical Interview Workshop\nHave a technical interview coming up? Not sure how to prepare?\n\nRegister for WIT'S Mock Interview Workshop to hear about tips & tricks on how to ace technical interviews and book a session for a one-on-one mock technical interview with an industry representative. Get ready to receive specific and personalised feedback on how you can improve your tech interview game with industry experts from Airwallex!\n\nNOTE: For this event, you will only be required to attend the opening presentation and the session you have been allocated. If a registrant does not show up, the position will be given to the student next in line. You can also register to attend the opening presentation only.\n\nRegistration link: https://forms.gle/G2LNTQXgg59WCuMMA\nWIT Presents: Mock Technical Interview Workshop\nHave a technical interview coming up? Not sure how to prepare?\n\nRegister for WIT'S Mock Interview Workshop to hear about tips & tricks on how to ace technical interviews and book a session for a one-on-one mock technical interview with an industry representative. Get ready to receive specific and personalised feedback on how you can improve your tech interview game with industry experts from Airwallex!\n\nNOTE: For this event, you will only be required to attend the opening presentation and the session you have been allocated. If a registrant does not show up, the position will be given to the student next in line. You can also register to attend the opening presentation only.\n\nRegistration link: https://forms.gle/G2LNTQXgg59WCuMMA",
+      photo: {
+        src: '',
+        alt: '',
+      },
+    },
+  },
+  {
+    title: 'Short Description',
+    start: '2023-04-12T15:00:00',
+    end: '2023-04-12T18:30:00',
+    data: {
+      link: '',
+      location: '',
+      labels: ['just lorem'],
+      description: 'hi.',
+      photo: {
+        src: '',
+        alt: '',
+      },
+    },
+  },
+  {
+    title: 'Empty Workshop',
+    start: '2023-04-12T15:00:00',
+    end: '2023-04-12T18:30:00',
+    data: {
+      link: '',
+      location: '',
+      labels: [],
+      description: '',
+      photo: {
+        src: '',
+        alt: '',
+      },
+    },
+  },
+  {
+    title: 'Test Today',
+    start: '2023-04-07T15:00:00',
+    data: {
+      link: '',
+      location: '',
+      labels: ['sleep', 'eat', 'play', 'read', 'watch'],
+      description: '',
+      photo: {
+        src: '',
+        alt: '',
+      },
+    },
+  },
+  {
+    title: 'Test Ongoing',
+    start: '2023-04-07T15:00:00',
+    end: '2023-04-12T18:30:00',
+    data: {
+      link: '',
+      location: '',
+      labels: [],
+      description: '',
+      photo: {
+        src: '',
+        alt: '',
+      },
+    },
+  },
+  {
+    title: 'Test Ended',
+    start: '2023-04-02T15:00:00',
+    data: {
+      link: '',
+      location: '',
+      labels: [],
+      description: '',
+      photo: {
+        src: '',
+        alt: '',
+      },
+    },
+  },
+  {
+    title: 'lowercase',
+    start: '2023-04-02T15:00:00',
+    data: {
+      link: '',
+      location: '',
+      labels: [],
+      description: '',
+      photo: {
+        src: '',
+        alt: '',
+      },
+    },
+  },
+];
---
+++ frontend/styles/Timeline.module.tsx
@@ -0,0 +1,139 @@
+import styled from '@emotion/styled';
+import { styled as mui_styled } from '@mui/material/styles';
+import { Tabs, Tab } from '@mui/material';
+
+// styles available: https://github.com/fullcalendar/fullcalendar/tree/main/packages/core/src/styles
+export const StyledCalendar = styled.div`
+  .fc {
+    text-transform: capitalize;
+
+    & .fc-button-primary {
+      background-color: #e85f5c;
+      border-color: #e85f5c;
+      transition: all 200ms;
+    }
+
+    & .fc-button-primary:hover {
+      background-color: #ff6a67;
+      border-color: #ff6a67;
+      transition: all 200ms;
+    }
+
+    & .fc-button-primary:active {
+      background-color: #e85f5c;
+      border-color: #e85f5c;
+    }
+
+    & .fc-button-primary:focus {
+      box-shadow: 0 0 0 0.2rem #e85e5c92;
+      transition: all 200ms;
+    }
+
+    & .fc-button-primary:not(:disabled):active:focus,
+    .fc-button-primary:not(:disabled).fc-button-active:focus {
+      box-shadow: 0 0 0 0.2rem #ff6a67;
+      transition: all 200ms;
+    }
+
+    & .fc-day-today {
+      background-color: #31363839;
+    }
+
+    & .fc-event {
+      cursor: pointer;
+      user-select: none;
+      transition: all 200ms;
+
+      color: black;
+    }
+
+    & .fc-event:hover {
+      color: #feb14b;
+      background-color: #404040;
+    }
+
+    & .fc-daygrid-event-dot {
+      border: calc(var(--fc-daygrid-event-dot-width) / 2) solid #feb14b;
+      border-radius: calc(var(--fc-daygrid-event-dot-width) / 2);
+      transition: all 200ms;
+    }
+  }
+
+  .fc-h-event {
+    display: block;
+    border: 1px solid #efefef;
+    background-color: #efefef;
+    transition: 200ms;
+    overflow: hidden;
+
+    & .fc-event-main {
+      color: #404040;
+      transition: all 200ms;
+    }
+
+    & .fc-event-main:hover {
+      color: #feb14b;
+      background-color: #404040;
+    }
+  }
+`;
+
+// docs: https://mui.com/material-ui/react-tabs/
+interface StyledTabProps {
+  label: string;
+  icon: string | React.ReactElement;
+  iconPosition?: 'top' | 'bottom' | 'start' | 'end';
+}
+
+const tabHeight = 34; // default: '48px'
+const tabBorderRadius = 5;
+const tabIndicatorTopPadding = 34;
+const tabIndicatorBottomMargin = 13;
+
+export const StyledTabs = mui_styled(Tabs)({
+  borderBottom: '1px solid #e8e8e8',
+
+  '& .MuiTabs-indicator': {
+    zIndex: '0',
+    backgroundColor: '#404040',
+    paddingTop: tabIndicatorTopPadding,
+    marginBottom: tabIndicatorBottomMargin,
+    borderRadius: tabBorderRadius,
+  },
+});
+
+export const StyledTab = mui_styled((props: StyledTabProps) => (
+  <Tab disableRipple {...props} />
+))(({ theme }) => ({
+  zIndex: '1',
+  fontFamily: 'Montserrat, sans-serif',
+  fontSize: '15px',
+  fontWeight: theme.typography.fontWeightMedium,
+  color: '#313638',
+  borderRadius: tabBorderRadius,
+
+  marginRight: theme.spacing(1),
+  paddingRight: 10,
+  paddingLeft: 10,
+
+  minWidth: 0,
+  minHeight: tabHeight,
+  height: tabHeight,
+  transition: '200ms',
+
+  '&:hover': {
+    color: '#FEB14B',
+  },
+
+  '&.Mui-selected': {
+    color: '#FEB14B',
+    backgroundColor: 'transparent',
+  },
+
+  '&& > svg': {
+    marginRight: 3,
+    [theme.breakpoints.down('xs')]: {
+      marginRight: 1,
+    },
+  },
+}));
