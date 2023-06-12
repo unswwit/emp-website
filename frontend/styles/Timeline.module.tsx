56a445f963984f0d4c4693c22e0827d1f4980fab
ItsLame <37344470+ItsLame@users.noreply.github.com>
2023-06-12 11:27:45 +1000

    refactor(frontend): css custom colour properties [wit-110] (#19)

--- frontend/styles/Timeline.module.tsx
+++ frontend/styles/Timeline.module.tsx
@@ -91,11 +91,11 @@ const tabIndicatorTopPadding = 34;
 const tabIndicatorBottomMargin = 13;
 
 export const StyledTabs = mui_styled(Tabs)({
-  borderBottom: '1px solid #e8e8e8',
+  borderBottom: '1px solid var(--light)',
 
   '& .MuiTabs-indicator': {
     zIndex: '0',
-    backgroundColor: '#404040',
+    backgroundColor: 'var(--dark)',
     paddingTop: tabIndicatorTopPadding,
     marginBottom: tabIndicatorBottomMargin,
     borderRadius: tabBorderRadius,
@@ -109,7 +109,7 @@ export const StyledTab = mui_styled((props: StyledTabProps) => (
   fontFamily: 'Montserrat, sans-serif',
   fontSize: '15px',
   fontWeight: theme.typography.fontWeightMedium,
-  color: '#313638',
+  color: 'var(--dark)',
   borderRadius: tabBorderRadius,
 
   marginRight: theme.spacing(1),
@@ -122,11 +122,11 @@ export const StyledTab = mui_styled((props: StyledTabProps) => (
   transition: '200ms',
 
   '&:hover': {
-    color: '#FEB14B',
+    color: 'var(--yellow)',
   },
 
   '&.Mui-selected': {
-    color: '#FEB14B',
+    color: 'var(--yellow)',
     backgroundColor: 'transparent',
   },
 
