c6a834b0f6f51d58c3a08b85acdeec56cbce83ea
gracexkan <80564637+gracexkan@users.noreply.github.com>
2023-06-18 19:20:14 +1000

    fixed type error

--- frontend/lib/api.tsx
+++ frontend/lib/api.tsx
@@ -1,5 +1,9 @@
 import { createClient } from 'contentful';
 
+if (!process.env.NEXT_PUBLIC_CONTENTFUL_API_SPACE || !process.env.NEXT_PUBLIC_CONTENTFUL_API_TOKEN) {
+  throw new Error('Contentful API space is not defined');
+}
+
 const client = createClient({
   space: process.env.NEXT_PUBLIC_CONTENTFUL_API_SPACE,
   accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_API_TOKEN,
