bcbf5e767cd7c03d213ada9d569a20066571f10a
gracexkan <80564637+gracexkan@users.noreply.github.com>
2023-07-13 11:48:31 +1000

    chore: fixed linting

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -6,38 +6,57 @@ import { SocialIcon } from 'react-social-icons';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 
-
-
 export default function Footer() {
   return (
-    <><div className={styles.divider}>
-      <div className={styles.flex}>
-        <Image
-          src="/WIT-logo-white.png"
-          alt="UNSWWIT Logo"
-          className={styles.footerLogo}
-          width={50}
-          height={50}
-          priority />
-        <p>© UNSW Women in Technology 2023</p>
+    <>
+      <div className={styles.divider}>
+        <div className={styles.flex}>
+          <Image
+            src="/WIT-logo-white.png"
+            alt="UNSWWIT Logo"
+            className={styles.footerLogo}
+            width={50}
+            height={50}
+            priority
+          />
+          <p>© UNSW Women in Technology 2023</p>
+        </div>
+        <div className={styles.footerIcons}>
+          <Tooltip title="Instagram" arrow>
+            <IconButton>
+              <div>
+                <SocialIcon
+                  url="https://www.instagram.com/wit.unsw/?hl=en"
+                  network="instagram"
+                  bgColor="#F48B01"
+                />
+              </div>
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Facebook" arrow>
+            <IconButton>
+              <div>
+                <SocialIcon
+                  url="https://www.facebook.com/unsw.wit"
+                  network="facebook"
+                  bgColor="#F48B01"
+                />
+              </div>
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Discord" arrow>
+            <IconButton>
+              <div>
+                <SocialIcon
+                  url="https://discord.gg/XDr9qmtQ"
+                  network="discord"
+                  bgColor="#F48B01"
+                />
+              </div>
+            </IconButton>
+          </Tooltip>
+        </div>
       </div>
-      <div className={styles.footerIcons}>
-        <Tooltip title="Instagram" arrow>
-          <IconButton>
-            <div><SocialIcon url="https://www.instagram.com/wit.unsw/?hl=en" network="instagram" bgColor = "#F48B01" /></div>
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Facebook" arrow>
-          <IconButton>
-            <div><SocialIcon url="https://www.facebook.com/unsw.wit" network="facebook" bgColor = "#F48B01" /></div>
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Discord" arrow>
-          <IconButton>
-            <div><SocialIcon url="https://discord.gg/XDr9qmtQ" network="discord" bgColor = "#F48B01"/></div>
-          </IconButton>
-        </Tooltip>
-      </div>
-    </div></>
+    </>
   );
 }
