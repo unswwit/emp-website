42e1d2d02316442b9398d6d7e7cf778573d23e69
ItsLame <37344470+ItsLame@users.noreply.github.com>
2023-09-05 10:38:04 +1000

    feat(frontend): login page (UI) [wit-75] (#36)
    * feat: login page
    
    Includes endpoint and UI only. No backend yet. Added next-auth package, some images, and new colour variables.
    
    * feat: mobile support
    
    Added support for mobile and other sizes.
    
    * feat: register endpoint
    
    Added register endpoint. No UI. Added link from login page to register page, and minor cosmetic update.
    
    * feat: login id validation
    
    Validate login id (Email or zID) via input pattern.
    
    * feat: background decorative elements
    
    * chore: linting
    
    * fix: right panel image sizing
    
    * fix: dark mode
    
    * fix: login email pattern validation

---
+++ frontend/pages/user/login.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Montserrat } from '@next/font/google';
+import { signIn } from 'next-auth/react';
+import styles from '../../styles/User.module.css';
+
+const montserrat = Montserrat({ subsets: ['latin'] });
+const monsterratBold = Montserrat({ weight: '700', subsets: ['latin'] });
+
+export default function login() {
+  return (
+    <div className={styles.user}>
+      <main className={montserrat.className}>
+        <div className={styles.panel}>
+          <div className={styles.left}>
+            <div className={styles.content}>
+              <h1>Sign in</h1>
+              <div className={styles.auth}>
+                {/* guide: https://mattermost.com/blog/add-google-and-github-login-to-next-js-app-with-nextauth/ */}
+                <button
+                  className={montserrat.className}
+                  onClick={() => signIn('google')}
+                >
+                  <img src="/google.svg" alt="google logo" />
+                  Sign in with Google
+                </button>
+              </div>
+              <div className={styles.dividerLabel}>
+                <hr />
+                OR
+                <hr />
+              </div>
+              <form method="POST">
+                <div>
+                  <label>Email or zID</label>
+                  <input
+                    required
+                    className={montserrat.className}
+                    type="text"
+                    id="id"
+                    name="id"
+                    placeholder="Enter your email or zID"
+                    pattern="[a-zA-Z0-9_\.]+@[a-zA-Z0-9]+(\.[a-zA-Z]+)+$|^z[0-9]{7}$"
+                    title="name@example.com or z1234567"
+                  />
+                </div>
+                <div>
+                  <label>Password</label>
+                  <input
+                    required
+                    className={montserrat.className}
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="Enter your password"
+                  />
+                </div>
+                <hr />
+                <button className={montserrat.className} type="submit">
+                  Log in
+                </button>
+              </form>
+              <p>
+                Haven't registered yet?{' '}
+                <a href="/user/register" className={monsterratBold.className}>
+                  Sign up
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className={styles.right}>
+            <img src="/login/image.png" alt="woman engineers" />
+          </div>
+        </div>
+      </main>
+      <div className={styles.bg}>
+        <img className={styles.decor1} src="/login/bottom-left.svg" />
+        <img className={styles.decor2} src="/login/top-right.svg" />
+      </div>
+    </div>
+  );
+}
---
+++ frontend/pages/user/register.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Montserrat } from '@next/font/google';
+import styles from '../../styles/User.module.css';
+
+const montserrat = Montserrat({ subsets: ['latin'] });
+
+export default function register() {
+  return (
+    <div className={styles.user}>
+      <main className={montserrat.className}></main>
+    </div>
+  );
+}
