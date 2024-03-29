b38e2da215fde9948bd88d32c3c61d7c0a8407d0
Kimberly <z5462253@ad.unsw.edu.au>
2024-03-30 02:11:02 +1100

    Created Countdown Feature
    Created the countdown feature where it will display the countdown

--- frontend/components/Countdown.tsx
+++ frontend/components/Countdown.tsx
@@ -1,18 +1,27 @@
 import styles from '../styles/Home.module.css';
-import { compareAsc } from 'date-fns';
+import { compareAsc, getWeekYear } from 'date-fns';
 import CountDown from './Count.js';
 
-export default function Countdown() {
-  const current_date = new Date();
-  const targetDate = new Date('2023-11-22T00:00:00Z');
-  const variable = compareAsc(current_date, new Date(2023, 10, 5, 2, 0, 0));
+export default function Countdown({date}: {date: Date}) {
+  const targetDate = new Date('2024-04-22T00:00:00Z');
+  const variable = compareAsc(date, targetDate);
+
+
   // today's date is after the date given by developer
   if (variable === 1) {
-    return (
-      <div className={styles.divider}>
-        <h2>Sign ups for 2023 have closed.</h2>
-      </div>
-    );
+    if (getWeekYear(targetDate) == 2023 && getWeekYear(date) >= 2024) {
+      return (
+        <div className={styles.divider}>
+          <h2>Sign ups for 2024 coming soon.</h2>
+        </div>
+      );
+    } else {
+      return (
+        <div className={styles.divider}>
+          <h2>Sign ups for 2023 have closed.</h2>
+        </div>
+      );
+    }
   } else {
     return (
       <div className={styles.divider}>
