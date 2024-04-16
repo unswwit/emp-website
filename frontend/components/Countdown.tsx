ac61aefc299b8a3792221394174ab91684f9256c
kdco4643 <127580898+kdco4643@users.noreply.github.com>
2024-04-16 12:14:52 +1000

    feat(frontend): signups countdown display [wit-180] (#79)
    * Created Countdown Feature
    
    Created the countdown feature where it will display the countdown
    
    * Fixed Spacing issue For Lint
    
    * Made changes based on the comments
    
    * Updated Count.js For changing The Text for Countdown
    
    * Centered Countdown Values
    
    ---------
    
    Co-authored-by: ItsLame <37344470+ItsLame@users.noreply.github.com>

--- frontend/components/Countdown.tsx
+++ frontend/components/Countdown.tsx
@@ -1,18 +1,27 @@
 import styles from '../styles/Home.module.css';
-import { compareAsc } from 'date-fns';
+import { compareAsc, getWeekYear } from 'date-fns';
 import CountDown from './Count.js';
 
 export default function Countdown() {
-  const current_date = new Date();
-  const targetDate = new Date('2023-11-22T00:00:00Z');
-  const variable = compareAsc(current_date, new Date(2023, 10, 5, 2, 0, 0));
+  const targetDate = new Date('2024-04-22T00:00:00Z');
+  const currentDate = new Date();
+  const compareDate = compareAsc(currentDate, targetDate);
+
   // today's date is after the date given by developer
-  if (variable === 1) {
-    return (
-      <div className={styles.divider}>
-        <h2>Sign ups for 2023 have closed.</h2>
-      </div>
-    );
+  if (compareDate === 1) {
+    if (getWeekYear(targetDate) === getWeekYear(currentDate)) {
+      return (
+        <div className={styles.divider}>
+          <h2>Sign ups for {getWeekYear(currentDate)} coming soon.</h2>
+        </div>
+      );
+    } else {
+      return (
+        <div className={styles.divider}>
+          <h2>Sign ups for {getWeekYear(currentDate)} have closed.</h2>
+        </div>
+      );
+    }
   } else {
     return (
       <div className={styles.divider}>
