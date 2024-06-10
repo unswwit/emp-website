b5fc1052fe7bb345b39e9ec78f1f4378b2e5ef83
Merlte <wstly92@gmail.com>
2024-06-10 18:56:09 +1000

    feat: basic LogHours page (WIP)

---
+++ frontend/components/LogHours.tsx
@@ -0,0 +1,55 @@
+import styles from '../styles/LogHours.module.css';
+import Button from '@mui/material/Button';
+
+export default function Hours() {
+    return (
+        <div className={styles.page}>
+            {/* <div className={styles.heroText}> */}
+            <h1>
+                Approve/Reject Hours
+            </h1>
+            {/* </div> */}
+            <div className={styles.sectionTable}>
+                <table>
+                    <colgroup>
+                        <col style={{ width: "8%" }} />
+                        <col style={{ width: "14%" }} />
+                        <col style={{ width: "33%" }} />
+                        <col style={{ width: "6%" }} />
+                        <col style={{ width: "3%" }} />
+                        <col style={{ width: "15%" }} />
+                    </colgroup>
+                    <thead>
+                        <tr>
+                            <th>Timestamp</th>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Evidence</th>
+                            <th>Hrs</th>
+                            <th>Verdict</th>
+                        </tr>
+                    </thead>
+                    {/* loop data */}
+                    <tbody>
+                        <tr>
+                            <td>02-16 20:38:40</td>
+                            <td><a href="#">FirstName MiddleName LastName</a></td>
+                            <td>Logging hours for the Empowerment Mentoring Program in WIT with my mentor</td>
+                            <td><a href="#">view.pdf</a></td>
+                            <td>24</td>
+                            <td><Button variant="contained" color="success" style={{ margin: "1px" }}>Approve</Button> <Button variant="contained" color="error" style={{ margin: "1px" }}>Reject</Button></td>
+                        </tr>
+                        <tr>
+                            <td>02-16 21:38:40</td>
+                            <td><a href="#">FirstName1 MiddleName1 LastName1</a></td>
+                            <td>AAAAA</td>
+                            <td><a href="#">view.pdf</a></td>
+                            <td>24</td>
+                            <td><Button variant="contained" color="success" style={{ margin: "1px" }}>Approve</Button> <Button variant="contained" color="error" style={{ margin: "1px" }}>Reject</Button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
---
+++ frontend/pages/index copy.tsx
@@ -0,0 +1,114 @@
+import Head from 'next/head';
+import { Montserrat } from 'next/font/google';
+import styles from '../styles/Home.module.css';
+import React from 'react';
+import Navbar from '../components/NavBar';
+import Hero from '../components/Hero';
+import Countdown from '../components/Countdown';
+import SponsorCollage from '../components/SponsorCollage';
+import Timeline from '../components/Timeline';
+// import Testimonials from '../components/Testimonials';
+import Footer from '../components/Footer';
+import { filterSponsors } from '../lib/helpers/sponsor';
+import { ITypeSponsorsFields, TypeSponsorsSkeleton } from '../types/sponsors';
+import ContentService from '../lib/api';
+import { GetStaticProps } from 'next';
+
+type Sponsors = {
+  sponsors: ITypeSponsorsFields[];
+};
+
+const montserrat = Montserrat({ subsets: ['latin'] });
+
+export default function Home({ sponsors }: Sponsors) {
+  return (
+    <div className={styles.home}>
+      <Head>
+        <title>WIT Empowerment Mentoring</title>
+        <meta
+          name="description"
+          content="women in technology empowerment website"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/WIT-logo-black.png" />
+      </Head>
+      <main className={montserrat.className}>
+        <Navbar />
+        <Hero />
+        <Countdown />
+        <div id="About" className={styles.section}>
+          <h1>EMPOWERMENT MENTORING</h1>
+          <p>
+            WIT's Empowerment Mentoring Program, run over Term 2 and 3, offers a
+            unique opportunity for students to enhance their skills and connect
+            with industry professionals. Prepare to level up your interview
+            skills, master technical interviews, and strengthen your
+            communication, teamwork, and leadership abilities.
+          </p>
+          <p>
+            As a mentee, you'll be paired with an experienced industry mentor
+            who will provide personalized guidance on starting your professional
+            career. With a wide range of mentor companies, you'll gain exposure
+            to diverse professional environments and expand your professional
+            network. By completing the program, you'll also earn AHEGS
+            accreditation, a recognised achievement by UNSW that showcases your
+            commitment to ongoing growth and development.
+          </p>
+          <p>
+            Don't miss this opportunity to empower yourself and thrive in the
+            world of technology!
+          </p>
+          <div className={styles.wrapper}>
+            <a
+              target="_blank"
+              href="https://www.facebook.com/events/545237864225826"
+            >
+              <button className={styles.button}>Learn More</button>
+            </a>
+          </div>
+        </div>
+        {/* <div id="Testimonials" className={styles.graySection}>
+          <h1>TESTIMONIALS</h1>
+          <Testimonials />
+        </div> */}
+        <div className={styles.divider}>
+          <h2>Sign up to the reminder list here:</h2>
+          <a
+            target="_blank"
+            href="https://www.facebook.com/events/545237864225826"
+          >
+            <button className={styles.button}>Remind me</button>
+          </a>
+        </div>
+        <div id="Timeline" className={styles.section}>
+          <h1>TIMELINE</h1>
+          <Timeline />
+        </div>
+        <div className={styles.divider}>
+          <h2>Mentor list will be released soon...</h2>
+        </div>
+        <div className={styles.section}>
+          <h1>SPONSORS AND AFFILIATIONS</h1>
+          <div className={styles.wrapper}>
+            <SponsorCollage tempSponsors={sponsors} />
+          </div>
+        </div>
+        <Footer />
+      </main>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const s = (
+    await ContentService.instance.getEntriesByType<TypeSponsorsSkeleton>(
+      'sponsors'
+    )
+  ).map((s) => s.fields);
+  const sponsors = filterSponsors(s);
+  return {
+    props: {
+      sponsors,
+    },
+  };
+};
