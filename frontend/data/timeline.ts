fb1a2400e14d5f06eb76d6837b2e90c57f6378d5
Sarah Ho <z5481589@ad.unsw.edu.au>
2024-07-18 20:19:50 +1000

    finished ticket

--- frontend/data/timeline.ts
+++ frontend/data/timeline.ts
@@ -1,6 +1,6 @@
 export const timeline = [
   {
-    title: 'Training Induction ',
+    title: 'Training Induction',
     start: '2023-06-08T10:00:00',
     end: '2023-06-08T23:59:00',
     data: {
@@ -36,10 +36,56 @@ export const timeline = [
     data: {
       link: 'TBA',
       location: 'TBA',
-      labels: ['compulsory'],
+      labels: ['2023', 'compulsory'],
       description: 'TBA',
       photo: {
         src: './placeholder-event.jpg',
+        alt: 'training-induction',
+      },
+    },
+  },
+  {
+    title: 'EMP Mentee Workshops',
+    year: '2024',
+    start: '2024-05-21T10:00:00',
+    end: '2024-05-21T23:59:00',
+    data: {
+      link: 'TBA',
+      location: 'TBA',
+      labels: ['2024', 'compulsory'],
+      description: 'TBA',
+      photo: {
+        src: './2024placeholder-event.jpg',
+        alt: 'training-induction',
+      },
+    },
+  },
+  {
+    title: 'EMP Training Day for Mentees',
+    start: '2024-06-18T10:00:00',
+    end: '2024-06-18T23:59:00',
+    data: {
+      link: 'TBA',
+      location: 'TBA',
+      labels: ['2024', 'compulsory'],
+      description: 'TBA',
+      photo: {
+        src: './2024placeholder-event.jpg',
+        alt: 'training-induction',
+      },
+    },
+  },
+  {
+    title: 'EMP Opening Ceremony',
+    start: '2024-06-18T10:00:00',
+    end: '2024-06-18T23:59:00',
+    data: {
+      link: 'TBA',
+      location: 'TBA',
+      labels: ['2024', 'compulsory'],
+      description: 'TBA',
+      photo: {
+        src: './2024placeholder-event.jpg',
         alt: 'training-induction',
       },
     },
