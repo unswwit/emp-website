61970bb9cb18bfe228f5394cd531f2c6437f84db
sarexakrish13 <100452633+sarexakrish13@users.noreply.github.com>
2024-09-16 18:16:34 +1000

    refactor/WIT-209 Define Sponor Type (#93)
    * WIT-209
    
    * WIT-209
    
    * removed eslint

--- frontend/lib/helpers/sponsor.ts
+++ frontend/lib/helpers/sponsor.ts
@@ -1,13 +1,15 @@
-/* eslint-disable */
-const filterSponsors = (sponsors: any) => {
-  const filteredSponsors = sponsors.filter((sponsor: any) => sponsor.year === 2023);
+
+import { Sponsor } from '../../types/Sponsor';
+
+const filterSponsors = (sponsors: Sponsor[]): { [key: string]: Sponsor[] } => {
+  const filteredSponsors = sponsors.filter((sponsor) => sponsor.year === 2023);
   const tempSponsors = {
-    'Diamond Sponsors': filteredSponsors.filter((sponsor: any) => sponsor.type === 'diamond'),
-    'Gold Sponsors': filteredSponsors.filter((sponsor: any) => sponsor.type === 'gold'),
-    'Silver Sponsors': filteredSponsors.filter((sponsor: any) => sponsor.type === 'silver'),
-    'Bronze Sponsors': filteredSponsors.filter((sponsor: any) => sponsor.type === 'bronze'),
-    Affiliations: filteredSponsors.filter((sponsor: any) => sponsor.type === 'affiliations'),
-    Partnerships: filteredSponsors.filter((sponsor: any) => sponsor.type === 'partnerships'),
+    'Diamond Sponsors': filteredSponsors.filter((sponsor) => sponsor.type === 'diamond'),
+    'Gold Sponsors': filteredSponsors.filter((sponsor) => sponsor.type === 'gold'),
+    'Silver Sponsors': filteredSponsors.filter((sponsor) => sponsor.type === 'silver'),
+    'Bronze Sponsors': filteredSponsors.filter((sponsor) => sponsor.type === 'bronze'),
+    Affiliations: filteredSponsors.filter((sponsor) => sponsor.type === 'affiliations'),
+    Partnerships: filteredSponsors.filter((sponsor) => sponsor.type === 'partnerships'),
   };
   return tempSponsors;
 };
