d070e34b15c7be14c6b3946429b46c6c74e0d656
sunny chen <sunnyuqingchen@gmail.com>
2025-05-28 22:47:37 +1000

    implemented a simple progress bar tracker for mentee hours

---
+++ frontend/components/mentee/ProgressTracker.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import { hoursInfo, hoursStatus } from '../../types/hours';
+
+const StyledBox = styled(Paper)(({ theme }) => ({
+  borderRadius: theme.shape.borderRadius,
+
+  marginBottom: theme.spacing(2),
+  border: `1px solid ${theme.palette.divider}`,
+
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  padding: '30px 100px',
+}));
+
+const ProgressBar = styled(LinearProgress)({
+  width: '100%',
+  height: 40,
+  borderRadius: 20,
+  [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor: '#eee',
+  },
+  [`& .${linearProgressClasses.bar}`]: {
+    borderRadius: 20,
+    backgroundColor: '#FCB14C',
+  },
+});
+
+const LabelContainer = styled('div')({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  marginTop: '10px',
+});
+
+const ProgressTracker = ({ hours, statuses }: { hours: hoursInfo[]; statuses: hoursStatus[] }) => {
+  const [filteredHours, setFilteredHours] = useState([] as hoursInfo[]);
+
+  useEffect(() => {
+    if (hours?.length > 0) {
+      setFilteredHours(hours?.filter((h) => statuses.includes(h.status)));
+    }
+  }, [hours]);
+
+  const totalHours = 20;
+  const hoursCompleted = 12;
+  const hoursRemaining = totalHours - hoursCompleted;
+
+  return (
+    <StyledBox>
+      <ProgressBar variant="determinate" value={(hoursCompleted / totalHours) * 100} />
+      <LabelContainer>
+        <h4>Hours Completed: {hoursCompleted}</h4>
+        <h4>Hours Remaining: {hoursRemaining}</h4>
+      </LabelContainer>
+    </StyledBox>
+  );
+};
+
+export default ProgressTracker;
